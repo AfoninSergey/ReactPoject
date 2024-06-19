768fcb170a04755e5cd02568b8b46a4a4a78d44a
Afonin Sergey <afoninsergey@mail.ru>
2024-06-19 19:21:59 +0300

    "1. Введение в React / Задание #1 / Часть 2"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	const thisYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{thisYear}г.</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload',
+			),
+			createElement('a', {
+				className: 'App-link',
+				href: 'https://reactjs.org',
+				target: '_blank',
+				rel: 'noopener noreferrer',
+			}, 'Learn React'),
+			createElement('p', null, `${thisYear}г.`),
+		),
 	);
+
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<p>{thisYear}г.</p>
+	// 		</header>
+	// 	</div>
+	// );
 };
 // весь код декларативный?
